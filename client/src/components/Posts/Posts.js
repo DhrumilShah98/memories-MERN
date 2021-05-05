408106cf54df37f5cdbf9f93fd15578ff79ad7db
DhrumilShah <quixoteintern.java@gmail.com>
2021-05-05 12:30:19 +0530

    Folder structure created for memories client

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from './Post/Post.js';
+const Posts = () => {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+    );
+}
+
+export default Posts;
