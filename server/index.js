5fdc4ed73910c9b9916f19504bbabfc6c48b3195
DhrumilShah <quixoteintern.java@gmail.com>
2021-05-05 11:14:36 +0530

    Memories connected to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+dotenv.config();
+
+const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(DB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)); })
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
