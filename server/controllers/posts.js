20402c39fdf0ddfcb267ec73213da22e3b089dd6
DhrumilShah <quixoteintern.java@gmail.com>
2021-05-07 02:34:01 +0530

    Like post server logic implemented

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -47,3 +47,17 @@ export const deletePost = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 };
+
+export const likePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    try {
+        const updateLikeCount = { $inc: { likeCount: 1 } };
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, updateLikeCount, { new: true });
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
