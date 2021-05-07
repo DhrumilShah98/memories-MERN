53c275f3edd1fd0546dd11bef1b1764289c08b59
DhrumilShah <quixoteintern.java@gmail.com>
2021-05-07 12:22:26 +0530

    Navbar component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import memories from './images/memories.png';
-import Posts from './components/Posts/Posts.js';
-import Form from './components/Form/Form.js';
+import { Container, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import Navbar from './components/Navbar/Navbar';
 import useStyles from './styles.js';
 import { getPosts } from './actions/posts';
 
@@ -18,12 +18,9 @@ const App = () => {
 
     return (
         <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60px" />
-            </AppBar>
             <Grow in>
                 <Container>
+                    <Navbar />
                     <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts setCurrentId={setCurrentId} />
