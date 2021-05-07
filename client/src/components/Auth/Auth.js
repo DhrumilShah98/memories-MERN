475f825735b1be70954cfe8b68ecb0656383253d
DhrumilShah <quixoteintern.java@gmail.com>
2021-05-07 13:02:02 +0530

    Router added - Multipage application setup

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div>AUTH</div>
+    );
+}
+
+export default Auth;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Toolbar, Typography, Button } from '@material-ui/core';
 import memories from '../../images/memories.png';
 import useStyles from './styles.js';
 
 const Navbar = () => {
     const classes = useStyles();
 
+    const user = null;
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-            <img className={classes.image} src={memories} alt="memories" height="60px" />
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60px" />
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+                            {user.result.name.charAt(0)}
+                        </Avatar>
+                        <Typography className={classes.userName} variant="h6">
+                            {user.result.name}
+                        </Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">
+                            Logout
+                        </Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">
+                        Login
+                    </Button>
+                )}
+            </Toolbar>
         </AppBar>
     );
 };
